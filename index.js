47256365764254e314836231d79021f50f0871eb
ambiknds <53797801+ambiknds@users.noreply.github.com>
2023-06-29 20:07:36 +0000

    init

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-// index.js
-// where your node app starts
-
-// init project
-require('dotenv').config();
 var express = require('express');
 var app = express();
 
@@ -11,20 +6,23 @@ var app = express();
 var cors = require('cors');
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
-
 // http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-// your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
-});
-
 // listen for requests :)
-var listener = app.listen(process.env.PORT || 3000, function () {
+var listener = app.listen(process.env.PORT || 3000, function() {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+//Request Header Parser Microservice
+
+app.get('/api/whoami', (req, res) => {
+
+  res.json({
+    ipaddress: req.socket.remoteAddress,
+    language: req.headers['accept-language'],
+    software: req.headers['user-agent']
+  })
+})
